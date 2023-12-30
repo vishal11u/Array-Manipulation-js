09289842738589303221f8a58e619a921fcb9564
Vishal_121-11 <tradersprofitclub110@gmail.com>
2023-12-30 14:09:07 +0530

    change

--- ArrayShift.js
+++ ArrayShift.js
@@ -1,11 +1,9 @@
-let arr=[1,"a",2,"b",3,"c"]
-console.log(arr.unshift([0]));
-// [ 0, 1, "a", 2, "b", 3, "c" ]
-// Unshift insert element in starting in array
+let obj=[1,2,3,4,5]
+console.log(obj.shift());
+//output: 1
+// Removing first element in array and returns
 
-let jrr=[[1,2],[2,3],[4,5]]
-console.log(jrr.unshift([1]));
-// [[ 1 ], [ 1, 2 ], [ 2, 3 ], [ 4, 5 ]]
-
-
-
+let jrr=["abc","xyz","arr"]
+console.log(jrr.shift());
+// output: "abc"
+//Removes the first element of an array if no argument is passed
