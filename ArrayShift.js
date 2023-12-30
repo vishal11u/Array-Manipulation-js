e5552d23c6b329b19e36dafeb6ebb3c1abff3eae
Vishal_121-11 <tradersprofitclub110@gmail.com>
2023-12-30 13:58:23 +0530

    change

--- ArrayShift.js
+++ ArrayShift.js
@@ -1,4 +1,11 @@
 let arr=[1,"a",2,"b",3,"c"]
 console.log(arr.unshift([0]));
 // [ 0, 1, "a", 2, "b", 3, "c" ]
+// Unshift insert element in starting in array
+
+let jrr=[[1,2],[2,3],[4,5]]
+console.log(jrr.unshift([1]));
+// [[ 1 ], [ 1, 2 ], [ 2, 3 ], [ 4, 5 ]]
+
+
 
